2499849c606ea10347c32e856dd134fea20db5ca
ntsikanesani <ntsikanesani@gmail.com>
2023-08-30 15:38:31 +0200

    Updated

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _strlen_recursion - prints the length of the string.
+ * _strlen_recursion - calculates the length of the string.
  * @s : the string to calculate.
  * return : an integer.
  */
